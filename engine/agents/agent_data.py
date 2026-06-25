5861814ba79f8bcadac45e91893a58a2251484d2
Vikas <vikasgoud1711@gmail.com>
2026-06-25 12:50:12 +0530

    V1

--- engine/agents/agent_data.py
+++ engine/agents/agent_data.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-import random
+
 
 @dataclass
 class Skill:
@@ -17,28 +17,25 @@ class CombatStats:
 @dataclass
 class AgentIdentityFormat:
     id : int
-    pos : tuple[int, int]
     role: str
     stats : CombatStats
     action_space_size:int=7
 
 class AgentIdentity:
-    def create_identity(self, agent_id : int, role: str = None, grid_size: int =20):  ## For now , later we use this to create diverse agent identities and base agent takes that and 
+    def create_identity(self, agent_id : int, role: str = None, ):  ## For now , later we use this to create diverse agent identities and base agent takes that and 
          ## the rule based or LLM agent with some sort of identity
         
-        _data = self._get_data(role,grid_size)
+        _data = self._get_data(role,)
         return AgentIdentityFormat(
             id = agent_id,
-            pos = _data['pos'],
             role = role,
             stats = CombatStats(hp = _data['hp'], max_hp=_data['max_hp'], skills=_data['skills'] )
         )
     
     @staticmethod
-    def _get_data(role : str,grid_size) ->dict:
+    def _get_data(role : str,) ->dict:
         if role == "Tank":
             return {
-                "pos": (0,random.randint(2,grid_size-1)),
                 "hp": 150,
                 "max_hp" : 150,
                 "skills" : {
@@ -49,33 +46,30 @@ class AgentIdentity:
 
         elif role == "Dealer":
             return {
-                "pos": (random.randint(0,grid_size-1), 1),
                 "hp": 80,
                 "max_hp" : 80,
                 "skills" : {
                     "basic_attack" :Skill(power=15,min_range=1,max_range=2,cooldown=0),
-                    "special" :Skill(power=35,min_range=2,max_range=4,cooldown=3),
+                    "special" :Skill(power=25,min_range=2,max_range=4,cooldown=3),
                 }
             }
         
         elif role == "Healer":
             return {
-                "pos": (0, 0),
                 "hp": 70,
                 "max_hp" : 70,
                 "skills" : {
-                    "heal" :Skill(power=-10,min_range=0,max_range=2,cooldown=1),
-                    "all_heal" :Skill(power=-30,min_range=1,max_range=4,cooldown=7),
+                    "heal" :Skill(power=-10,min_range=0,max_range=2,cooldown=2),
+                    "all_heal" :Skill(power=-30,min_range=1,max_range=4,cooldown=10),
                 }
             }
         elif role == "Boss":
             return {
-                "pos": (random.randint(14,grid_size-1), random.randint(14,grid_size)),
-                "hp": 300,
-                "max_hp" : 300,
+                "hp": 1000,
+                "max_hp" : 1000,
                 "skills" : {
-                    "basic_attack" :Skill(power=20,min_range=1,max_range=2,cooldown=1),
-                    "aoe" :Skill(power=15,min_range=1,max_range=4,cooldown=5),
+                    "basic_attack" :Skill(power=20,min_range=1,max_range=2,cooldown=3),
+                    "aoe" :Skill(power=40,min_range=1,max_range=4,cooldown=15),
                 }
             }
 
